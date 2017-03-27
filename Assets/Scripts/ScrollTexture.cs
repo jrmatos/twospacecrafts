cc8ab89cd41612417458001a41c626c0aae33b16
Paulo Matos <paulo.matos@itriad.org.br>
2017-03-27 10:30:55 -0400

    ScrollTexture added on background

--- Assets/Scripts/ScrollTexture.cs
+++ Assets/Scripts/ScrollTexture.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ScrollTexture : MonoBehaviour {
+
+	[Header("Scene References")]
+	//public Material material;
+	MeshRenderer meshRenderer;
+	Material material;
+
+	[Header("Velocity")]
+	public Vector2 scrollVelocity;
+	public Vector2 scrollOffset;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {	
+		scrollOffset = Vector2.zero;
+
+		meshRenderer = GetComponent<MeshRenderer> ();
+		material = meshRenderer.materials[0];
+
+		material.mainTextureOffset = scrollOffset;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		material.mainTextureOffset = scrollOffset;
+
+		scrollOffset += scrollVelocity * Time.deltaTime;
 	}
 }
