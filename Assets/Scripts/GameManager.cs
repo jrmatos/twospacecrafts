5d78b2df1dc8d057e971570d1567d7c6fa5499fa
Paulo Matos <paulojomatos@gmail.com>
2017-03-28 00:49:58 -0400

    UI added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public bool gameover = false;
+	public Text gameoverText;
+	public Text scoreText;
+	public Text gameoverScoreText;
+	public Text gameoverHighScoreText;
+	private ScoreManager scoreManager;
+	CanvasGroup restartButtonCanvasGroup;
 
 	private float initialTimeScale;
 
 	// Use this for initialization
 	void Start () {
 		initialTimeScale = Time.timeScale;
+		scoreManager = GameObject.FindObjectOfType<ScoreManager> ();
+		restartButtonCanvasGroup = GameObject.Find("RestartButton").GetComponent<CanvasGroup>();
 	}
 
 	public bool isGameover(){
@@ -22,6 +31,7 @@ public class GameManager : MonoBehaviour {
 
 		if (this.gameover) {
 			Time.timeScale = 0;
+			showGameoverHUDs ();
 		} else {
 			Time.timeScale = initialTimeScale;
 		}
@@ -30,5 +40,20 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void showGameoverHUDs() {
+
+		gameoverScoreText.text = "Score: " + scoreManager.getScore();
+		gameoverHighScoreText.text = "High Score: " + scoreManager.getHighScore();
+
+		scoreText.GetComponent<Text> ().enabled = false;
+		gameoverText.GetComponent<Text> ().enabled = true;
+		gameoverScoreText.GetComponent<Text> ().enabled = true;
+		gameoverHighScoreText.GetComponent<Text> ().enabled = true;
+
+		restartButtonCanvasGroup.alpha = 1f;
+		restartButtonCanvasGroup.interactable = true;
+		restartButtonCanvasGroup.blocksRaycasts = true;
 	}
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour {
 
 	private Text text;
-	private int score;
+	private int score = 0;
+	private int highScore = 0;
 
 	// Use this for initialization
 	void Start () {
 		text = GetComponent<Text> ();
-		score = 0;
 		text.text = "" + score;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public int getHighScore() {
+		return highScore;
 	}
 
 	public void AddScore(int points){
 		score += points;
 		text.text = "" + score;
+
+		if (score > highScore) {
+			highScore = score;
+		}
 	}
 }
